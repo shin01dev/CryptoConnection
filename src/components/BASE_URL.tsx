08e35ab81d4ed5d0f92da7b2428373269f7756e2
shin01dev <wonking9218@gmail.com>
2023-09-18 22:06:21 +0900

    first commit

--- src/components/BASE_URL.tsx
+++ src/components/BASE_URL.tsx
@@ -1,2 +1,2 @@
 // constants.js
-export const BASE_URL = 'https://crypto-community-git-main-shin01dev.vercel.app/';
+export const BASE_URL = 'https://crypto-community-git-main-shin01dev.vercel.app';
