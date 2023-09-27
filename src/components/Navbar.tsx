2dee7985def63eed4d559041e90830a8277b5b26
shin01dev <wonking9218@gmail.com>
2023-09-27 22:32:10 +0900

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,14 +8,14 @@ import SearchBar from './SearchBar';
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
   return (
-    <div className='top-0 inset-x-0 h-fit bg-gradient-to-l from-purple-600 border-b border-zinc-300 z-[10] py-2'>
+<div className='top-0 inset-x-0 h-fit lg:bg-gradient-to-l lg:from-purple-600 border-b border-zinc-300 z-[10] py-2 
+   bg-gradient-to-l  from-purple-300 border-b border-zinc-300 '>
       <div className='max-w-full md:mr-4 h-full mx-auto flex flex-col md:flex-row items-center justify-between gap-2 px-2 md:px-0'>
         <div className="flex gap-2 items-center">
           {/* logo */}
           <a href='/'>
             <div className="flex gap-2 items-center cursor-pointer">
-              <img src='/favicon.ico' className='h-8 w-8 md:h-12 md:w-10 ml-3' />
-              <p className='text-zinc-700 text-sm font-medium'>크립토 커넥션 </p>
+              <img src='/logo-4.png' className='h-12 w-40 ml-3' />
             </div>
           </a>
           {/* actions */}
