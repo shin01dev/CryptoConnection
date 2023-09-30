24b2cb6823aee770a13518d393e5fcb7215c8b79
shin01dev <wonking9218@gmail.com>
2023-09-30 14:22:04 +0900

    first commit

--- src/components/BASE_URL.tsx
+++ src/components/BASE_URL.tsx
@@ -1,3 +1,3 @@
 // constants.js
-export const BASE_URL = 'https://crypto-community-git-main-shin01dev.vercel.app';
+export const BASE_URL = 'https://crypto-connection-5jh6-git-main-shin01dev.vercel.app';
 // export const BASE_URL = 'http://localhost:3000';
