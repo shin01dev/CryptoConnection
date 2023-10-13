bc57c51883a86dd3fa8d920f0aaf54f60fb412de
shin01dev <138709684+shin01dev@users.noreply.github.com>
2023-10-13 16:36:26 +0900

    first commit

--- src/components/BASE_URL.tsx
+++ src/components/BASE_URL.tsx
@@ -1,5 +1,5 @@
 // constants.js
 // export const BASE_URL = 'https://crypto-connection-5jh6-git-main-shin01dev.vercel.app';
 
-export const BASE_URL = 'http://localhost:3000';
-// export const BASE_URL='https://www.cryptoconnection.club';
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL='https://www.cryptoconnection.club';
